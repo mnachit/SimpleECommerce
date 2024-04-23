e739a5a46287624ea29c6feffc5c1507b726664f
mnachit <nachitmed70@gmail.com>
2024-04-23 10:04:42 +0100

    first commit

---
+++ src/main/java/com/example/simpleecommerce/SimpleECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleecommerce/SimpleECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
